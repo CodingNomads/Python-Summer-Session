1d9a4cabc6002f2ec83bbabcce860e3abac246e2
Jon Fincher <jfincher42@gmail.com>
2025-07-24 17:56:16 -0500

    Adding Wee4 sample code

---
+++ Week4/mylib.py
@@ -0,0 +1,3 @@
+# Comment
+def product(x, y):
+    return x * y
---
+++ Week4/week4.py
@@ -0,0 +1,80 @@
+# import requests
+
+# r = requests.get("http://www.python.org")
+# print(r.status_code)
+
+# import math
+
+# from math import sqrt
+
+# def sqrt(x):
+#     return x
+
+# x = sqrt(10)
+# print(x)
+
+# import mylib
+
+# p = mylib.product(3, 5)
+# print(p)
+
+# squares = []
+# for x in range(1, 11):
+#     if x % 2 == 0:
+#         squares.append(x**2)
+
+# print(squares)
+
+# c_squares = [x**2 for x in range(1,11) if x % 2 == 0]
+# print(c_squares)
+
+# pairs = [(x, y) for x in range(3) for y in range(3)]
+# print(pairs)
+
+# for x in range(3):
+#     for y in range(3):
+#         pairs.append( (x,y) )
+
+
+# def double(x):
+#     return x * 2
+
+# double = lambda x: x * 2
+
+# # print(double(4))
+
+# print((lambda x: x * 2))
+
+# nums = [1, 2, 3]
+# doubled = list(map(lambda x: x * 2, nums))
+
+# print(doubled)
+# evens = list(filter(lambda x: x % 2 == 0, nums))
+# print(evens)
+
+# names = ["Jon", "David", "Guillaume"]
+# name_len = 4
+# long_names = list(filter(lambda foo: len(foo) > name_len, names))
+# print(long_names)
+
+
+# print("Hello")
+
+# for x in range(5, -5, -1):
+#     try:
+#         print(f"Result = {25/x}")
+#     except ZeroDivisionError:
+#         print("You are not Chuck Norris!")
+#     finally:
+#         print("Whew!")
+
+# try:
+#     divide = [25 / x for x in range(5, -5, -1) if x != 0]
+# except ZeroDivisionError:
+#     print("You are not Chuck Norris!")
+
+# print(divide)
+
+# print((lambda x: x * 3)(2))
+
+import antigravity
